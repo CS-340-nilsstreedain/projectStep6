e435c441a95bfbbc7dbdba5a9a06a7052eae0cf1
Nils Streedain <streedan@oregonstate.edu>
2023-05-11 22:52:17 -0700

    Added forever

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6784;
 const navItems = [
 	{ title: 'Home', url: '/', active: true },
 	{ title: 'Customers', url: '/customers' },
