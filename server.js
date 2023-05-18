94c70ed23df4e6daab7d1359a18e03ebe9c9efca
Nils Streedain <nils@streedain.com>
2023-05-18 01:00:44 -0700

    Refacroted forms and added columns from server.js

--- server.js
+++ server.js
@@ -3,13 +3,68 @@ const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 6784;
 const pages = [
-	{title: 'Home', url: '/'},
-	{title: 'Customers', url: '/customers'},
-	{title: 'Orders', url: '/orders'},
-	{title: 'OrderBooks', url: '/orderbooks'},
-	{title: 'Books', url: '/books'},
-	{title: 'BookGenres', url: '/bookgenres'},
-	{title: 'Genres', url: '/genres'}
+	{
+		title: 'Home',
+		url: '/'
+	},
+	{
+		title: 'Customers',
+		url: '/customers',
+		columns: [
+			{name: 'name', description: 'Full Name', type: 'input'},
+			{name: 'phone', description: 'Phone Number', type: 'input'},
+			{name: 'email', description: 'Email Address', type: 'input'},
+			{name: 'address', description: 'Home Address', type: 'input'},
+			{name: 'genre', description: 'Favorite Genre', type: 'input'},
+			{name: 'genre', description: 'Favorite Genre', type: 'select'}
+		]
+	},
+	{
+		title: 'Orders',
+		url: '/orders',
+		columns: [
+			{name: 'customer', description: 'Customer', type: 'select'},
+			{name: 'date', description: 'Date', type: 'input'},
+			{name: 'total', description: 'Total', type: 'input'}
+		]
+	},
+	{
+		title: 'OrderBooks',
+		url: '/orderbooks',
+		columns: [
+			{name: 'order', description: 'Order ID', type: 'input'},
+			{name: 'book', description: 'Book', type: 'select'},
+			{name: 'quantity', description: 'Quantity', type: 'input'},
+			{name: 'price', description: 'Price', type: 'input'}
+		]
+	},
+	{
+		title: 'Books',
+		url: '/books',
+		columns: [
+			{name: 'title', description: 'Title', type: 'input'},
+			{name: 'author', description: 'Author', type: 'input'},
+			{name: 'price', description: 'Price', type: 'input'},
+			{name: 'stock', description: 'Stock', type: 'input'}
+		]
+	},
+	{
+		title: 'BookGenres',
+		url: '/bookgenres',
+		columns: [
+			{name: 'book', description: 'Book', type: 'select'},
+			{name: 'genre', description: 'Genre', type: 'select'}
+		]
+		
+	},
+	{
+		title: 'Genres',
+		url: '/genres',
+		columns: [
+		   {name: 'name', description: 'Name', type: 'input'},
+		   {name: 'Description', description: 'Description', type: 'input'}
+		]
+	}
 ];
 
 // Create an Express app
@@ -34,10 +89,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Create routes for each page
-pages.forEach(({title, url}) => {
+pages.forEach(({title, url, columns}) => {
     app.get(url, (req, res) => {
         res.render(title.toLowerCase(), {
             title: title,
+			columns: columns,
             pages
         });
     });
