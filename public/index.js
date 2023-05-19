51816fe6f378c46ffb69992c3d3bff3c8f5a81cb
Nils Streedain <nils@streedain.com>
2023-05-18 17:49:57 -0700

    Added color accents & table checkboxes

--- public/index.js
+++ public/index.js
@@ -16,3 +16,18 @@ editButton.addEventListener('click', () => {
 deleteButton.addEventListener('click', () => {
 	deleteDialog.showModal();
 });
+
+// Select all check box in table
+var boxes = document.getElementsByClassName('select-row');
+document.getElementById('select-all').addEventListener('change', function(e) {
+	for(var i = 0; i < boxes.length; i++)
+		boxes[i].checked = e.target.checked;
+});
+
+// De-select "select all" if one box unchecked.
+for(var i = 0; i < boxes.length; i++) {
+	boxes[i].addEventListener('change', function(e) {
+		if(!e.target.checked)
+			document.getElementById('select-all').checked = false;
+	});
+}
