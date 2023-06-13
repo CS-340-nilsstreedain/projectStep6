96f1b5090c8fbad558b6b77dc2843a7e3544c61b
Nils Streedain <nils@streedain.com>
2023-06-12 23:24:13 -0700

    Cleanup/commenting

--- public/index.js
+++ public/index.js
@@ -1,132 +1,150 @@
+// Get dialog elements
 const createDialog = document.getElementById('createForm');
 const editDialog = document.getElementById('editForm');
 const deleteDialog = document.getElementById('deleteForm');
 
+// Add event listener for 'create' button click
 document.getElementById('create').addEventListener('click', () => {
-	createDialog.showModal();
+    createDialog.showModal(); // Show create dialog
 });
 
+// Add event listener for 'createClose' button click
 document.getElementById('createClose').addEventListener('click', () => {
-	createDialog.close();
+    createDialog.close(); // Close create dialog
 });
 
+// Add event listener for 'edit' button click
 document.getElementById('edit').addEventListener('click', () => {
-	editDialog.showModal();
+    editDialog.showModal(); // Show edit dialog
 });
 
+// Add event listener for 'editClose' button click
 document.getElementById('editClose').addEventListener('click', () => {
-	editDialog.close();
+    editDialog.close(); // Close edit dialog
 });
 
+// Add event listener for 'delete' button click
 document.getElementById('delete').addEventListener('click', () => {
-	deleteDialog.showModal();
+    deleteDialog.showModal(); // Show delete dialog
 });
 
+// Add event listener for 'deleteClose' button click
 document.getElementById('deleteClose').addEventListener('click', () => {
-	deleteDialog.close();
+    deleteDialog.close(); // Close delete dialog
 });
 
-// Select all check box in table
+// Select all checkbox in table
 var boxes = document.getElementsByClassName('select-row');
 document.getElementById('select-all').addEventListener('change', function(e) {
-	for(var i = 0; i < boxes.length; i++)
-		boxes[i].checked = e.target.checked;
+    for(var i = 0; i < boxes.length; i++)
+        boxes[i].checked = e.target.checked;
 });
 
 // De-select "select all" if one box unchecked.
 for(var i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener('change', function(e) {
-		if(!e.target.checked)
-			document.getElementById('select-all').checked = false;
-	});
+    boxes[i].addEventListener('change', function(e) {
+        if(!e.target.checked)
+            document.getElementById('select-all').checked = false;
+    });
 }
 
+// Get form elements from dialogs
 const createForm = createDialog.querySelector('form');
 const editForm = editDialog.querySelector('form');
 const deleteForm = deleteDialog.querySelector('form');
 
+// Add event listener to createForm submit
 createForm.addEventListener('submit', event => {
-	event.preventDefault();
-	let dataObj = {};
-	for (let i = 0; i < createForm.elements.length; i++)
-		if (createForm.elements[i].name)
-			dataObj[createForm.elements[i].name] = createForm.elements[i].value;
-	
-	fetch(window.location.pathname + '/create', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(dataObj)
-	})
-	.then(response => {
-		if (!response.ok) {
-			// If HTTP status is not ok (e.g. 500), get the error message from the response
-			return response.json().then(errData => {
-				throw new Error(errData.error);
-			});
-		}
-		// If HTTP status is ok, reload the page
-		window.location.reload();
-	})
-	.catch(error => {
-		// Catch any error that happened along the way
-		document.getElementById('createError').innerText = 'There is a problem with your input:\n' + error.message;
-	});
-});
+    event.preventDefault(); // Prevent form submission
+    let dataObj = {};
 
-editForm.addEventListener('submit', event => {
-	event.preventDefault();
-	let dataObj = {};
-	for (let i = 0; i < editForm.elements.length; i++)
-		if (editForm.elements[i].name)
-			dataObj[editForm.elements[i].name] = editForm.elements[i].value;
+    // Iterate through form elements and populate dataObj
+    for (let i = 0; i < createForm.elements.length; i++)
+        if (createForm.elements[i].name)
+            dataObj[createForm.elements[i].name] = createForm.elements[i].value;
     
-	fetch(window.location.pathname + '/update', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(dataObj)
-	})
-	.then(response => {
-		if (!response.ok) {
-			// If HTTP status is not ok (e.g. 500), get the error message from the response
-			return response.json().then(errData => {
-				throw new Error(errData.error);
-			});
-		}
-		// If HTTP status is ok, reload the page
-		window.location.reload();
-	})
-	.catch(error => {
-		// Catch any error that happened along the way
-		document.getElementById('editError').innerText = 'There is a problem with your input:\n' + error.message;
-	});
+    // Send POST request with dataObj
+    fetch(window.location.pathname + '/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataObj)
+    })
+    .then(response => {
+        if (!response.ok) {
+            // If HTTP status is not ok (e.g. 500), get the error message from the response
+            return response.json().then(errData => {
+                throw new Error(errData.error);
+            });
+        }
+        // If HTTP status is ok, reload the page
+        window.location.reload();
+    })
+    .catch(error => {
+        // Catch any error that happened along the way
+        document.getElementById('createError').innerText = 'There is a problem with your input:\n' + error.message;
+    });
 });
 
-deleteForm.addEventListener('submit', event => {
-	event.preventDefault();
-	const selectedRows = document.querySelectorAll('input.select-row:checked');
-	const ids = Array.from(selectedRows).map(row => row.parentElement.parentElement.getAttribute('data-id'));
+// Add event listener to editForm submit
+editForm.addEventListener('submit', event => {
+    event.preventDefault(); // Prevent form submission
+    let dataObj = {};
 
-	ids.forEach(id => {
-		fetch(window.location.pathname + '/delete/' + id, {
-			method: 'POST'
-		})
-		.then(response => {
-			if (!response.ok) {
-				// If HTTP status is not ok (e.g. 500), get the error message from the response
-				return response.json().then(errData => {
-					throw new Error(errData.error);
-				});
-			}
-			// If HTTP status is ok, reload the page
-			window.location.reload();
-		})
-		.catch(error => {
-			// Catch any error that happened along the way
-			document.getElementById('deleteError').innerText = 'There was a problem communicating with the database:\n' + error.message;
-		});
-	});
+    // Iterate through form elements and populate dataObj
+    for (let i = 0; i < editForm.elements.length; i++)
+        if (editForm.elements[i].name)
+            dataObj[editForm.elements[i].name] = editForm.elements[i].value;
+    
+    // Send POST request with dataObj
+    fetch(window.location.pathname + '/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataObj)
+    })
+    .then(response => {
+        if (!response.ok) {
+            // If HTTP status is not ok (e.g. 500), get the error message from the response
+            return response.json().then(errData => {
+                throw new Error(errData.error);
+            });
+        }
+        // If HTTP status is ok, reload the page
+        window.location.reload();
+    })
+    .catch(error => {
+        // Catch any error that happened along the way
+        document.getElementById('editError').innerText = 'There is a problem with your input:\n' + error.message;
+    });
+});
+
+// Add event listener to deleteForm submit
+deleteForm.addEventListener('submit', event => {
+    event.preventDefault(); // Prevent form submission
+    const selectedRows = document.querySelectorAll('input.select-row:checked');
+    const ids = Array.from(selectedRows).map(row => row.parentElement.parentElement.getAttribute('data-id'));
+
+    ids.forEach(id => {
+        // Send POST request to delete specific item
+        fetch(window.location.pathname + '/delete/' + id, {
+            method: 'POST'
+        })
+        .then(response => {
+            if (!response.ok) {
+                // If HTTP status is not ok (e.g. 500), get the error message from the response
+                return response.json().then(errData => {
+                    throw new Error(errData.error);
+                });
+            }
+            // If HTTP status is ok, reload the page
+            window.location.reload();
+        })
+        .catch(error => {
+            // Catch any error that happened along the way
+            document.getElementById('deleteError').innerText = 'There was a problem communicating with the database:\n' + error.message;
+        });
+    });
 });
--- server.js
+++ server.js
@@ -66,41 +66,61 @@ const app = express();
 // Initialize an instance of express-handlebars
 const hbs = exphbs.create({});
 
-// Register handlebars helper functions for template manipulation
+// Register handlebars helper function for checking equality
 hbs.handlebars.registerHelper('isEqual', function(a, b, options) {
-	return a === b ? options.fn(this) : options.inverse(this);
+    // Check if a is equal to b and return the appropriate template block
+    return a === b ? options.fn(this) : options.inverse(this);
 });
 
+// Register handlebars helper function for retrieving entry keys
 hbs.handlebars.registerHelper('entryKeys', function(a, b) {
+    // Check if the number of keys for the specified entry is 1 and Return the value of the single key
     if (IDs[b].keys.length == 1)
         return a[IDs[b].keys[0]];
     else
+        // Return a concatenated string of two foreign keys
         return a[IDs[b].foriegnKeys[0].key] + "_" + a[IDs[b].foriegnKeys[1].key];
 });
 
+// Register handlebars helper function for checking if a key is the primary key of an entity
 hbs.handlebars.registerHelper('entityKey', function(a, b) {
-	if (IDs[a].keys.length === 1 && IDs[a].keys[0] === b)
-		return true;
-	return false;
-});
-
-hbs.handlebars.registerHelper('multipleChoice', function(a, b) {
-    for (let i = 0; i < IDs[a].foriegnKeys.length; i++)
-        if (IDs[a].foriegnKeys[i].name == b)
-            return true;
+    // Check if the entity has a single primary key and if it matches the provided key
+    if (IDs[a].keys.length === 1 && IDs[a].keys[0] === b)
+        // Return true if the key matches the primary key
+        return true;
+    // Return false if the key doesn't match the primary key or there are multiple primary keys
     return false;
 });
 
+// Register handlebars helper function for checking if a key is a multiple choice key
+hbs.handlebars.registerHelper('multipleChoice', function(a, b) {
+    // Loop through the foreign keys of the specified entity
+    for (let i = 0; i < IDs[a].foriegnKeys.length; i++)
+        // Check if the name of the foreign key matches the provided key
+        if (IDs[a].foriegnKeys[i].name == b)
+            // Return true if the key is a multiple choice key
+            return true;
+    // Return false if the key is not a multiple choice key
+    return false;
+});
+
+// Register handlebars helper function for looking up and iterating over an array of items
 hbs.handlebars.registerHelper('lookupAndEach', function(context, key, options) {
+    // Retrieve the array of items from the context using the specified key
     const items = context[key];
+    // Initialize an empty string to store the rendered output
     let out = "";
 
+    // Check if the items is an array
     if(Array.isArray(items))
+        // Loop through each item in the array and render the block of the template for each item
         for(let i = 0; i < items.length; i++)
             out += options.fn(items[i]);
 
+    // Return the rendered output
     return out;
 });
+
 
 // Set 'hbs' as the templating engine for the Express app
 app.engine('hbs', exphbs.engine({extname: '.hbs'}));
@@ -134,55 +154,67 @@ function renderPage(res, title, tables, tableResults, error) {
 
 // Function to handle SQL statements for Update and Delete operations
 const handleSQLStatement = (action, req, res, url, title) => {
+    // Retrieve the keys of the request body object
     const keys = Object.keys(req.body);
+    // Generate the SQL SET clause by mapping the keys and values of the request body
     const sqlSet = keys.slice(IDs[title].keys.length).map(key => `${key} = '${req.body[key]}'`).join(', ');
-    
+
     let sqlConditions = "";
+    // Check if the entity has a single primary key
     if (IDs[title].keys.length == 1) {
+        // Handle SQL conditions for single primary key
         if (action === 'UPDATE')
             sqlConditions = IDs[title].keys[0] + " = " + req.body[IDs[title].keys[0]];
         else if (action === 'DELETE FROM')
             sqlConditions = IDs[title].keys[0] + " = " + req.body.id;
     } else {
+        // Handle SQL conditions for multiple primary keys
         if (action === 'UPDATE')
             sqlConditions = IDs[title].foriegnKeys[0].key + " = " + req.body[IDs[title].foriegnKeys[0].key] + " AND " + IDs[title].foriegnKeys[1].key + " = " + req.body[IDs[title].foriegnKeys[1].key];
         else if (action === 'DELETE FROM')
             sqlConditions = IDs[title].foriegnKeys[0].key + " = " + req.body.id.split("_")[0] + " AND " + IDs[title].foriegnKeys[1].key + " = " + req.body.id.split("_")[1];
-        
     }
-    
+
+    // Generate the complete SQL statement
     const sql = `${action} ${title} ${action === 'UPDATE' ? 'SET ' + sqlSet : ''} WHERE ${sqlConditions}`;
-    
+
+    // Call the modQuery function with the SQL statement, transformed values, and response object
     modQuery(sql, Object.values(req.body).toString().split("_").map(Number), res, url);
 }
 
 // Create routes for each page
 pages.forEach(({title, url}) => {
-	if (title == 'Home') {
-		app.get(url, (req, res) => {
-			res.render('home', {
-				title: title,
-				pages
-			});
-		});
-	} else {
-		app.get(url, (req, res) => {
+    if (title == 'Home') {
+        // Route for the home page
+        app.get(url, (req, res) => {
+            // Render the home template with the title and pages data
+            res.render('home', {
+                title: title,
+                pages
+            });
+        });
+    } else {
+        // Route for other pages
+        app.get(url, (req, res) => {
+            // Execute a SQL query to select all data from the specified table
             db.pool.query(`SELECT * FROM ${title}`, (error, tableResults) => {
-				if (error)
-					console.log(error.sqlMessage);
-				
+                if (error)
+                    console.log(error.sqlMessage);
+
+                // Query the INFORMATION_SCHEMA to retrieve foreign key information
                 db.pool.query(`SELECT COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME
                     FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
                     WHERE TABLE_NAME = ? AND REFERENCED_TABLE_NAME IS NOT NULL`, [title], (error, results) => {
-                    
+
                     // If the table doesn't have foreign keys, render the page without foreign key data
                     if (results.length === 0)
                         return renderPage(res, title, null, tableResults, error);
-                    
+
+                    // Retrieve foreign key data for each foreign key reference
                     let foreignKeyPromises = results.map(result => {
                         let foreignTableName = result.REFERENCED_TABLE_NAME;
                         let columnName = result.COLUMN_NAME;
-                        
+
                         // Query the foreign table
                         return new Promise((resolve, reject) => {
                             db.pool.query(`SELECT * FROM ${foreignTableName}`, function(error, foreignTableResults) {
@@ -198,88 +230,111 @@ pages.forEach(({title, url}) => {
                         });
                     });
 
+                    // Retrieve all foreign key data asynchronously
                     Promise.all(foreignKeyPromises)
                         .then(allForeignKeys => {
                             let tables = Object.assign({}, ...allForeignKeys); // Combine all key-value pairs into one object
-                            
+
                             // Format date column if it exists
                             if (tableResults[0].date)
                                 for (let i = 0; i < tableResults.length; i++)
                                     if (tableResults[i].date instanceof Date)
                                         tableResults[i].date = tableResults[i].date.toISOString();
-  
+
+                            // Render the page with foreign key data
                             renderPage(res, title, tables, tableResults, error);
                         })
                         .catch(error => console.log(error));
                 });
-			});
-		});
-	}
-	
+            });
+        });
+    }
+    
     // CREATE
     app.post(url + '/create', (req, res) => {
+        // Retrieve the keys and values from the request body
         const keys = Object.keys(req.body);
         const values = Object.values(req.body);
         const sql = `INSERT INTO ${title} (${keys.join(',')}) VALUES (${keys.map(() => '?').join(',')})`;
 
+        // Execute the modQuery function to perform the SQL insert operation
         modQuery(sql, values, res, url);
     });
 
     // UPDATE
     app.post(url + '/update', (req, res) => {
+        // Handle the SQL update statement for the specified table
         handleSQLStatement('UPDATE', req, res, url, title);
     });
 
     // DELETE
     app.post(url + '/delete/:id', (req, res) => {
+        // Set the id parameter in the request body
         req.body.id = req.params.id;
+        // Handle the SQL delete statement for the specified table
         handleSQLStatement('DELETE FROM', req, res, url, title);
     });
 });
 
+
 // SEARCH Books By Genre
 app.post('/books/search', (req, res) => {
+    // Retrieve the genreID from the request body
     let genreID = req.body.genreID;
     let sql = '';
     let params = [];
 
+    // Check if genreID is provided
     if (genreID) {
-        genreID = '%' + genreID + '%';  // Wrap the genre name with '%' for SQL's LIKE
+        // Wrap the genre name with '%' for SQL's LIKE
+        genreID = '%' + genreID + '%';
         sql = `SELECT * FROM Books
             JOIN BookGenres ON Books.bookID = BookGenres.bookID
             JOIN Genres ON BookGenres.genreID = Genres.genreID
             WHERE Genres.name LIKE ?`;
         params = [genreID];
-    } else
+    } else {
         sql = `SELECT * FROM Books`;
-    
+    }
+
+    // Execute the SQL query with the provided parameters
     db.pool.query(sql, [genreID], (error, results) => {
-        if (error) res.status(500).json({error: error.sqlMessage});
-        else res.json(results);
+        if (error)
+            res.status(500).json({ error: error.sqlMessage });
+        else
+            res.json(results);
     });
 });
 
 // SEARCH Customers By Name
 app.post('/customers/search', (req, res) => {
+    // Retrieve the name from the request body
     let name = req.body.name;
     let sql = '';
     let params = [];
 
+    // Check if name is provided
     if (name) {
-        name = '%' + name + '%';  // Wrap the genre name with '%' for SQL's LIKE
+        // Wrap the name with '%' for SQL's LIKE
+        name = '%' + name + '%';
         sql = `SELECT * FROM Customers
             WHERE Customers.name LIKE ?`;
         params = [name];
-    } else
+    } else {
         sql = `SELECT * FROM Customers`;
-    
+    }
+
+    // Execute the SQL query with the provided parameters
     db.pool.query(sql, [name], (error, results) => {
-        if (error) res.status(500).json({error: error.sqlMessage});
-        else res.json(results);
+        if (error)
+            res.status(500).json({ error: error.sqlMessage });
+        else
+            res.json(results);
     });
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, function (err) {
-	if(err) throw err;
+    if (err) throw err;
     console.log(`Server running on port ${PORT}`);
 });
